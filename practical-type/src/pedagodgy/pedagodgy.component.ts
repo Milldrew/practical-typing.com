0e394d33bff1ac5b3484216188a91b1a654e61bc
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2024-12-04 23:57:18 -0700

    📅  | Wed Dec  4 23:57:18 MST 2024

--- practical-type/src/pedagodgy/pedagodgy.component.ts
+++ practical-type/src/pedagodgy/pedagodgy.component.ts
@@ -4,8 +4,9 @@ const PEDAGODGY_TEXT = `
    - Makes speed measurements more meaningful.
    - Allows for fair competition between typists.
    - Focuses the mind on the key strokes and improves mind, muscle memory, and myelination.
+   - Should result in higher typing speeds due to the lack of time wasted on determining the next key to press.
 2. Non readable text is used to focus the mind on the key strokes and not the content.
-    `;
+`;
 
 @Component({
   standalone: true,
