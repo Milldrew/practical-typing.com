df99c39397bbd627761fdc029b654fbcc5cad27b
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2024-12-05 00:50:43 -0700

    📅  | Thu Dec  5 00:50:43 MST 2024

--- practical-type/src/home/home.component.ts
+++ practical-type/src/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TimerComponent} from '../timer/timer.component';
+import {TextControlsService} from '../text-controls/text-controls.service';
+import {TextControlsComponent} from '../text-controls/text-controls.component';
+import {TimerService} from '../timer/timer.service';
+import {SpeedTypingTextDirective} from '../speed-typing-text/speed-typing-text.directive';
+
 
 @Component({
   selector: 'practical-home',
-  imports: [],
+  imports: [TimerComponent, TextControlsComponent, SpeedTypingTextDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(
+    public textControlsService: TextControlsService,
+    public timerService: TimerService
+  ) {}
 
 }
