bdd36ceb3a8312a74caa60e5f2caa43fe3ff8938
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2024-12-10 16:53:25 -0700

    build

--- practical-type/build-gh-pages.sh
+++ practical-type/build-gh-pages.sh
@@ -1,7 +1,7 @@
 npx ng build --base-href "/practical-typing.com/" ;
 cp -r  ../docs/browser/* ../docs/ ;
 rm -r ../docs/browser ;
-cp ../docs/index.html ../docs/404.html ;
+# cp ../docs/index.html ../docs/404.html ;
 cd .. ; git add . ; git commit -m "build" ;
 git push ;
 git push --tags  ;
