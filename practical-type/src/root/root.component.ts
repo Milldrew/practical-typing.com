ab6489144c5bc052e1521d553d14c9211b66e3f2
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2025-03-16 05:39:04 -0600

    📅  | Sun Mar 16 05:39:04 MDT 2025

--- practical-type/src/root/root.component.ts
+++ practical-type/src/root/root.component.ts
@@ -1,25 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink, RouterModule} from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {TimerService} from '../timer/timer.service';
 import {KeyboardDataService} from '../keyboard-page/keyboard-data.service';
+import {MatSnackBarModule, MatSnackBar, } from '@angular/material/snack-bar';
 import {ScoresService} from '../scores/scores.service';
+import {GlobalEventEmitter, SHOW_SNACKBAR} from '../eventz/global.event-emitter';
 
 @Component({
   standalone: true,
   selector: 'practical-root',
-  imports: [RouterModule, MatMenuModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [RouterModule, MatMenuModule, MatButtonModule, MatIconModule, RouterLink, MatSnackBarModule],
   templateUrl: './root.component.html',
   styleUrl: './root.component.scss'
 })
 export class RootComponent {
   constructor(
+    private snackBar: MatSnackBar,
     public timerService: TimerService,
     private keyboardDataService: KeyboardDataService,
     private scoresService: ScoresService
-  ) {}
+  ) {
+    GlobalEventEmitter.on(SHOW_SNACKBAR, (message: string) => {
+      this.snackBar.open(message, 'Close', {
+        duration: 4000,
+      });
+    });
+  }
 
 
   getYear() {
